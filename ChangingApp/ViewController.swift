83c3b4a159932dffe0ea33af37999014c1a7091e
Ronald Hollander <ronaldhollander@Ronalds-MacBook-Pro.local>
2018-01-26 13:44:27 +0100

    Addded Icons; Simple Logic on Button to change icon

--- ChangingApp/ViewController.swift
+++ ChangingApp/ViewController.swift
@@ -12,14 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func changeIcon(_ sender: Any) {
+        if UIApplication.shared.supportsAlternateIcons {
+            if UIApplication.shared.alternateIconName == nil {
+                UIApplication.shared.setAlternateIconName("Moon")
+            } else {
+                UIApplication.shared.setAlternateIconName(nil)
+            }
+        }
     }
-
-
 }
 
